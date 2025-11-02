2a7dc51f14d484706e43b2c7a4d5008424378767
madmax63-git <maximilian.maurer2025@gmail.com>
2025-11-02 14:24:49 -0500

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by <madmax63-git>
+ # Here is another update by <madmax63-git>
